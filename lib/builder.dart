b22c45800b8c3a9936da41c7e2b3e110f9a5ae99
Faisal Ramdan <faisalramdan.id@gmail.com>
2021-06-02 20:06:06 +0700

    Resolved annotation for `class AppLocalizationsDelegate issue

--- lib/builder.dart
+++ lib/builder.dart
@@ -2,5 +2,5 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:sheet_localization_generator/sheet_localization_generator.dart';
 
-Builder flutterSheetLocalization(BuilderOptions options) => SharedPartBuilder(
-    [SheetLocalizationGenerator()], 'flutter_sheet_localization');
+Builder flutterSheetLocalization(BuilderOptions options) =>
+    SharedPartBuilder([SheetLocalizationGenerator()], 'sheet_localization');
